ab8c9a2074c2a13820b0950b235d6ef4f02cf1ca
ShoppingLiao <tom0801a@gmail.com>
2020-05-03 20:15:49 +0800

    feat: test simple-list

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # shoppingliao.github.io
 
 放一些專案的小東西
+
 - [簡單計算機](https://shoppingliao.github.io/Simple-Calculator)
+- [簡單清單](https://shoppingliao.github.io/Simple-List)
---
+++ Simple-List/precache-manifest.26d4cb8427a192c298b6265627ef9c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f282abe1dd189822f82012883a142ac",
+    "url": "/Simple-List/index.html"
+  },
+  {
+    "revision": "8490cf11807b7ed32bed",
+    "url": "/Simple-List/static/css/main.aef27dfe.chunk.css"
+  },
+  {
+    "revision": "94027792dc1d90b1f73f",
+    "url": "/Simple-List/static/js/2.579ff33d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-List/static/js/2.579ff33d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8490cf11807b7ed32bed",
+    "url": "/Simple-List/static/js/main.8b925908.chunk.js"
+  },
+  {
+    "revision": "39135f514874fd17cf93",
+    "url": "/Simple-List/static/js/runtime-main.9852fd9d.js"
+  }
+]);
