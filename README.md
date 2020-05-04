f505997e46da8dc5c07ab7d04c050525974ebbd1
ShoppingLiao <tom0801a@gmail.com>
2020-05-04 22:41:39 +0800

    feat: 新增蒐尋器葉面

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - [簡單計算機](https://shoppingliao.github.io/Simple-Calculator)
 - [簡單清單](https://shoppingliao.github.io/Simple-List)
+- [簡單 Youtube 搜尋清單](https://shoppingliao.github.io/Simple-Youtube-Search-List)
---
+++ Simple-Youtube-Search-List/precache-manifest.1bbd8cbe9defc3d9656d5af87288a712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ac81d97d6d8d397ae1a3e87d8e6fb9",
+    "url": "/Simple-Youtube-Search-List/index.html"
+  },
+  {
+    "revision": "1e48e87928b1abb8c10d",
+    "url": "/Simple-Youtube-Search-List/static/css/main.b26faf39.chunk.css"
+  },
+  {
+    "revision": "0324428ca4241cf4aa71",
+    "url": "/Simple-Youtube-Search-List/static/js/2.a4391c6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-Youtube-Search-List/static/js/2.a4391c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e48e87928b1abb8c10d",
+    "url": "/Simple-Youtube-Search-List/static/js/main.8e263181.chunk.js"
+  },
+  {
+    "revision": "7af46025e43ed21b271d",
+    "url": "/Simple-Youtube-Search-List/static/js/runtime-main.274c3135.js"
+  }
+]);
