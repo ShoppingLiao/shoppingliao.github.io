1c29744575d09c6fdf9c59887adceb30770705c6
Shopping Liao <tom0801a@gmail.com>
2020-08-04 12:52:53 +0800

    tfeat: add simple-raging-app

---
+++ Simple-Rating-App/precache-manifest.a03c0532ecaf3e4b43b9b015b9577434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e3c3f0f876208244ccacdfba4a47a0",
+    "url": "/Simple-Rating-App/index.html"
+  },
+  {
+    "revision": "b86c76778d34af084659",
+    "url": "/Simple-Rating-App/static/js/2.cbbd8364.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Simple-Rating-App/static/js/2.cbbd8364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9623ca758d52ef0f2ef8",
+    "url": "/Simple-Rating-App/static/js/main.204be0ef.chunk.js"
+  },
+  {
+    "revision": "dbd8e5a88a153705c696",
+    "url": "/Simple-Rating-App/static/js/runtime-main.441ac0f8.js"
+  }
+]);
